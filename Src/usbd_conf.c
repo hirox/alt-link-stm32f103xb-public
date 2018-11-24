0926333198847737bb280c0f89457b521ef18a86
Hiromichi Goto <hirox777@gmail.com>
2018-11-24 19:20:38 +0900

    Add comments

--- Src/usbd_conf.c
+++ Src/usbd_conf.c
@@ -238,6 +238,10 @@ void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef *hpcd)
                        LL Driver Interface (USB Device Library --> PCD)
 *******************************************************************************/
 
+// PMA: Packet Memoery Area
+// PCD: Peripheral Controller Driver
+// MSP: MCU Support Package
+
 // 6 eps * 4 bytse (ADDR/COUNT) * 2 entries (TX/RX) => 48bytes (512-48=464, 464-64*6=80, 80-8*2=64)
 #define PMA_ADDR_BASE (6 * 4 * 2)
 
@@ -255,22 +259,25 @@ USBD_StatusTypeDef USBD_LL_Init(USBD_HandleTypeDef *pdev)
   hpcd.Init.phy_itface = PCD_PHY_EMBEDDED;
   hpcd.Init.speed = PCD_SPEED_FULL;
   hpcd.Init.low_power_enable = 0;
-  
+
   /* Link The driver to the stack */
   hpcd.pData = pdev;
   pdev->pData = &hpcd;
-  
+
   /* Initialize LL Driver */
   HAL_PCD_Init((PCD_HandleTypeDef*)pdev->pData);
 
+  /* Control OUT(0x00)/IN(0x80): 32/32bytes */
   HAL_PCDEx_PMAConfig(pdev->pData , 0x00 , PCD_SNG_BUF, PMA_ADDR_BASE);
   HAL_PCDEx_PMAConfig(pdev->pData , 0x80 , PCD_SNG_BUF, PMA_ADDR_BASE + 0x20);
+  /* HID IN/OUT: 64/64bytes */
   HAL_PCDEx_PMAConfig(pdev->pData , CUSTOM_HID_EPIN_ADDR , PCD_SNG_BUF, PMA_ADDR_BASE + 0x40 * 1);
   HAL_PCDEx_PMAConfig(pdev->pData , CUSTOM_HID_EPOUT_ADDR , PCD_SNG_BUF, PMA_ADDR_BASE + 0x40 * 2);
+  /* CDC IN/OUT/CMD: 64/64/8bytes */
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_IN_EP , PCD_SNG_BUF, PMA_ADDR_BASE + 0x40 * 3);
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_OUT_EP , PCD_SNG_BUF, PMA_ADDR_BASE + 0x40 * 4);
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_CMD_EP , PCD_SNG_BUF, PMA_ADDR_BASE + 0x40 * 5);
-
+  /* CDC2 IN/OUT/CMD: 64/64/8bytes */
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_IN_EP2 , PCD_SNG_BUF, PMA_ADDR_BASE + 0x08 + 0x40 * 5);
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_OUT_EP2 , PCD_SNG_BUF, PMA_ADDR_BASE + 0x08 + 0x40 * 6);
   HAL_PCDEx_PMAConfig(pdev->pData , CDC_CMD_EP2 , PCD_SNG_BUF, PMA_ADDR_BASE + 0x08 + 0x40 * 7);
